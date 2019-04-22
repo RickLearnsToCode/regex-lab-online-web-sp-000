43152b10d490a36778b7ed786272495813bfd17f
Rick Rosetti <rick.rosetti@gmail.com>
2019-04-22 14:56:10 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,17 @@
 def starts_with_a_vowel?(word)
-
+  /^[aeiouAEIOU].*/.match(word) != nil ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\S*ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+/\A[A-Z]\w+\p{P}/.match(text) != nil ? true : false
 end
 
 def valid_phone_number?(phone)
